736a65f32e4dc30a2a7fa55b130d2911bc945026
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-24 14:22:09 +0300

    Removed some of the markup from the component

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,9 +1,9 @@
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 const ChatListScreen = () => {
     return (
         <View>
-
+            <Text>Something in the way</Text>
         </View>
     )
 }
