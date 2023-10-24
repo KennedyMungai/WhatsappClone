73e03dc61582e325fd4844b367c10b89275811bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-24 14:29:14 +0300

    Added some styles for teh markup

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const ChatListScreen = () => {
     return (
-        <View>
+        <View style={styles.chatScreenView}>
             <Text>Something in the way</Text>
         </View>
     )
 }
 
 export default ChatListScreen
+
+const styles = StyleSheet.create({
+    chatScreenView: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'black'
+    }
+})
