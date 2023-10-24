917d315fee65e96406ef49c5d6bedb20bad0e02a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-24 12:31:20 +0300

    Added an import for the expo font package

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar'
 import { useCallback, useEffect, useState } from 'react'
 import { StyleSheet, Text } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import * as Font from 'expo-font'
 
 SplashScreen.preventAutoHideAsync()
 
