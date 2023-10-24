0b637bfb0c9167bb60f4ed0fcd80de532b3da5cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-24 14:31:57 +0300

    Commented out some of the styles from the App component

--- App.js
+++ App.js
@@ -71,8 +71,8 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
+		// alignItems: 'center',
+		// justifyContent: 'center'
 	},
 	counterButtonsView: {
 		flexDirection: 'row',
