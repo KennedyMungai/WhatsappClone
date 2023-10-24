f850f4dcc5d726f071eeec9ea7bcef18f3fea811
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-24 11:27:05 +0300

    Added another button and deleted the text for the component

--- App.js
+++ App.js
@@ -6,13 +6,17 @@ export default function App() {
 	return (
 		<SafeAreaProvider style={styles.container}>
 			<SafeAreaView>
-				<Text>Open up App.js to start working on your app!</Text>
-				<StatusBar style='auto' />
+				<Button
+					title='Click Me'
+					onPress={() => console.log('Button Clicked')}
+				/>
 
 				<Button
 					title='Click Me'
 					onPress={() => console.log('Button Clicked')}
 				/>
+
+				<StatusBar style='auto' />
 			</SafeAreaView>
 		</SafeAreaProvider>
 	)
