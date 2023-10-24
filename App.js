2381140c90c61611c9b5bcce532145487e0fdd82
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-24 11:20:42 +0300

    Added the SafeAreaView component for the main view

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
-import { Button, StyleSheet, Text, View } from 'react-native'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Button, StyleSheet, Text } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
 	return (
 		<SafeAreaProvider style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style='auto' />
+			<SafeAreaView>
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style='auto' />
 
-			<Button
-				title='Click Me'
-				onPress={() => console.log('Button Clicked')}
-			/>
+				<Button
+					title='Click Me'
+					onPress={() => console.log('Button Clicked')}
+				/>
+			</SafeAreaView>
 		</SafeAreaProvider>
 	)
 }
