9f93374b1c32f2cd8583cb28a171670302ed23b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-25 15:59:11 +0300

    Added some new styles for the AuthScreen

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -13,7 +13,10 @@ export default AuthScreen
 
 const styles = StyleSheet.create({
     chatScreenView: {
-
+        margin: 10,
+        padding: 10,
+        flex: 1,
+        backgroundColor: 'white'
     }
 })
 
