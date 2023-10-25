6a1953eaed871bf5765b714b0a1a256ab4b2dd4a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-25 15:55:01 +0300

    Updated the logic for the AuthScreen of the application

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,11 +1,20 @@
-import { View } from "react-native"
+import { View, Text, StyleSheet } from 'react-native'
 
 const AuthScreen = () => {
     return (
-        <View>
-
+        <View style={styles.chatScreenView}>
+            <Text>Auth Screen</Text>
         </View>
     )
 }
 
 export default AuthScreen
+
+const styles = StyleSheet.create({
+    chatScreenView: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
