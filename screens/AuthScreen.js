d31a998e86a7900e7cd6ab5c69a90a62eca29136
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-26 10:20:35 +0300

    Formated the definition of the Input component

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -6,7 +6,12 @@ import { FontAwesome5 } from '@expo/vector-icons'
 const AuthScreen = () => {
     return (
         <SafeAreaView style={styles.chatScreenView}>
-            <Input label={"Something in the way"} IconPack={FontAwesome5} iconName={"user"} iconSize={24} />
+            <Input
+                label={"Something in the way"}
+                IconPack={FontAwesome5}
+                iconName={"user"}
+                iconSize={24}
+            />
         </SafeAreaView>
     )
 }
