f2b57b87d4d636555a876c5accdacde9be18eeef
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-26 11:47:45 +0300

    Got rid of all the unneeded fields

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,24 +1,10 @@
-import { Entypo, FontAwesome5, Zocial } from '@expo/vector-icons'
+import { Entypo, Zocial } from '@expo/vector-icons'
 import Input from '../components/Input'
 import SubmitButton from '../components/SubmitButton'
 
 const SignInForm = () => {
     return (
         <>
-            <Input
-                label={"FirstName"}
-                IconPack={FontAwesome5}
-                iconName={"user"}
-                iconSize={24}
-                placeholder={'First Name'}
-            />
-            <Input
-                label={"LastName"}
-                IconPack={FontAwesome5}
-                iconName={"user"}
-                iconSize={24}
-                placeholder={'Last Name'}
-            />
             <Input
                 label={"Email"}
                 IconPack={Zocial}
