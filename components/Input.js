d3baac5ed40a777aaea3350cfc486299cb2c78b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-27 10:44:26 +0300

    Spread out the props for the TextInput element

--- components/Input.js
+++ components/Input.js
@@ -32,6 +32,7 @@ const Input = (
                     style={styles.textInputStyles}
                     placeholder={placeholder}
                     onChangeText={onChangeText}
+                    {...props}
                 />
             </View>
             {
