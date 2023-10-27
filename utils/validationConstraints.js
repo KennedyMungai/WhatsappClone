f354b36f8590fdb1f84845557d6da9b24840999f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-27 10:34:24 +0300

    Created the function to validate the email with some dummy logic

--- utils/validationConstraints.js
+++ utils/validationConstraints.js
@@ -1,24 +1,43 @@
 import { validate } from 'validate.js'
 
 export const validateString = (id, value) => {
-    if (id === 'first-name' || id === 'last-name') {
-        const constraints = {
-            presence: {
-                allowEmpty: false,
-                message: "Can't be empty"
-            }
+    const constraints = {
+        presence: {
+            allowEmpty: false,
+            message: "Can't be empty"
         }
-
-        if (value !== '') {
-            constraints.format = {
-                pattern: '[a-z]+',
-                flag: 'i',
-                message: 'can only contain letters'
-            }
-        }
-
-        const validationResult = validate({ [id]: value }, { [id]: constraints })
-
-        return validationResult && validationResult[id]
     }
+
+    if (value !== '') {
+        constraints.format = {
+            pattern: '[a-z]+',
+            flag: 'i',
+            message: 'can only contain letters'
+        }
+    }
+
+    const validationResult = validate({ [id]: value }, { [id]: constraints })
+
+    return validationResult && validationResult[id]
+}
+
+export const validateEmail = (id, value) => {
+    const constraints = {
+        presence: {
+            allowEmpty: false,
+            message: "Can't be empty"
+        }
+    }
+
+    if (value !== '') {
+        constraints.format = {
+            pattern: '[a-z]+',
+            flag: 'i',
+            message: 'can only contain letters'
+        }
+    }
+
+    const validationResult = validate({ [id]: value }, { [id]: constraints })
+
+    return validationResult && validationResult[id]
 }
